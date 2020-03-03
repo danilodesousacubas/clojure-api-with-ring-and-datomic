91428429aa6d2b700473117b503922ec51ce1cd7
Danilo de Souza <danilodesousa@gmail.com>
2020-03-03 00:18:44 -0300

    Created model

--- store/src/store/model.clj
+++ store/src/store/model.clj
@@ -1,3 +1,8 @@
-(ns store.model)
+(ns store.model
+  (:use clojure.pprint)
+  (:require [datomic.api :as d]))
 
-
+(defn create-store [name email cnpj]
+  {:store/name name
+   store/email email
+   store/cnpj  cnpj})
